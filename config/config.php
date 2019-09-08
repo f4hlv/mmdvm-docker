899b4ecea255dd2896c6dbd2282387246e6b282d
Vincent <34775661+f4hlv@users.noreply.github.com>
2019-09-08 15:58:22 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,59 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("MMDVMLOGPATH", "/var/log/mmdvm");
+define("MMDVMINIPATH", "/config");
+define("MMDVMINIFILENAME", "MMDVM.ini");
+define("MMDVMHOSTPATH", "/MMDVMHost");
+define("ENABLEXTDLOOKUP", "on");
+define("TALKERALIAS", "on");
+define("DMRIDDATPATH", "");
+define("RESOLVETGS", "on");
+define("YSFGATEWAYLOGPATH", "/var/log/YSFGateway");
+define("YSFGATEWAYLOGPREFIX", "YSFGateway");
+define("YSFGATEWAYINIPATH", "/config");
+define("YSFGATEWAYINIFILENAME", "YSFGateway.ini");
+define("YSFHOSTSPATH", "/config");
+define("YSFHOSTSFILENAME", "YSFHosts.txt");
+define("ENABLEDMRGATEWAY", "on");
+define("DMRGATEWAYLOGPATH", "");
+define("DMRGATEWAYLOGPREFIX", "");
+define("DMRGATEWAYINIPATH", "");
+define("DMRGATEWAYPATH", "");
+define("DMRGATEWAYINIFILENAME", "");
+define("LINKLOGPATH", "");
+define("IRCDDBGATEWAY", "");
+define("TIMEZONE", "Europe/Paris");
+define("LOCALE", "fr_FR");
+define("LOGO", "");
+define("DMRPLUSLOGO", "");
+define("BRANDMEISTERLOGO", "");
+define("REFRESHAFTER", "120");
+define("SHOWCPU", "on");
+define("SHOWRPTINFO", "on");
+define("SHOWMODES", "on");
+define("SHOWLH", "on");
+define("SHOWLOCALTX", "on");
+define("SHOWPROGRESSBARS", "on");
+define("TEMPERATUREHIGHLEVEL", "65");
+define("SWITCHNETWORKUSER", "");
+define("SWITCHNETWORKPW", "");
+define("VIEWLOGUSER", "");
+define("VIEWLOGPW", "");
+define("HALTUSER", "");
+define("HALTPW", "");
+define("REBOOTUSER", "");
+define("REBOOTPW", "");
+define("RESTARTUSER", "");
+define("RESTARTPW", "");
+define("REBOOTYSFGATEWAY", "");
+define("REBOOTMMDVM", "");
+define("REBOOTSYS", "");
+define("HALTSYS", "");
+define("POWERONLINEPIN", "");
+define("POWERONLINESTATE", "");
+define("SHOWQRZ", "on");
+define("RSSI", "avg");
+?>
